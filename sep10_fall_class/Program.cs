15a81b39ad2a9f5f8ec821078ec5abd4fdd08388
Sai Akshith Thodupunuri <tsaiakshith@gmail.com>
2019-09-10 18:36:41 -0400

    Add project files.

---
+++ sep10_fall_class/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sep10_fall_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
