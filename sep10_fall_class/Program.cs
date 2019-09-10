5c26afa5b19809369b657c8e828c52a316a23e3f
Sai Akshith Thodupunuri <tsaiakshith@gmail.com>
2019-09-10 19:40:51 -0400

    rewrite

--- sep10_fall_class/Program.cs
+++ sep10_fall_class/Program.cs
@@ -6,7 +6,33 @@ namespace sep10_fall_class
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("How many years of experience do you have?");
+                int a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 0:
+                        Console.WriteLine("Needs lot of practise");
+                        break;
+
+                    case 1:
+                        Console.WriteLine("Looks like you have a littler expereince");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("WOW! You have been doing this for a little while");
+                        break;
+
+                    default:
+                        Console.WriteLine("You are an expert");
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Enter an integer and try again");
+            }
         }
     }
 }
